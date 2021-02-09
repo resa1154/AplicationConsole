c3118a2d3269c25fe8ab72f880647a95bebb5583
resaabdilah <resaabdilah11@gmail.com>
2021-02-09 09:37:07 +0700

    IntegerNumber

---
+++ IntegerNumber/IntegerNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IntegerNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int i = 1;
+            int j = 2;
+            int l = 1;
+            Console.Write("Masukkan Angka : ");
+            number = int.Parse(Console.ReadLine());
+
+            int number1 = number - i;
+            int number2 = number / j;
+            int number3 = (number - number2) / j;
+            int number4 = number / number;
+
+            Console.WriteLine(number);
+            Console.WriteLine(number1 + "," + i);
+            Console.WriteLine(number2 + "," + number2);
+            Console.WriteLine(number2 + "," + number3 + "," + number3);
+            while (l <= number)
+            {
+                Console.Write(number4 + ",");
+                l++;
+            }
+        }
+
+    }
+}
